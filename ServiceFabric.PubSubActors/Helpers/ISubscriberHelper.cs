2ac6d85c916079245e83216aa3b2d769fb06ee4b
Danijel <peric.danijel@gmail.com>
2019-02-03 14:46:43 +0100

    - Subscriber simplified

---
+++ ServiceFabric.PubSubActors/Helpers/ISubscriberHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ServiceFabric.PubSubActors.Helpers
+{
+    public interface ISubscriberHelper
+    {
+        /// <summary>
+        /// Registers this Actor as a subscriber for messages of type <paramref name="messageType"/> with the <see cref="BrokerService"/>.
+        /// </summary>
+        /// <returns></returns>
+        Task RegisterMessageTypeAsync(Type messageType, bool flushQueue, Uri brokerServiceName = null, string listenerName = null);
+
+        Task RegisterMessageTypeAsync(Type messageType, Uri brokerServiceName = null, string listenerName = null);
+
+        Task UnregisterMessageTypeAsync(Type messageType, Uri brokerServiceName = null);
+
+        Task UnregisterMessageTypeAsync(Type messageType, bool flushQueue, Uri brokerServiceName = null);
+    }
+}
